78709fe6e76d21767b52abfb8027313d63465f5c
Amanda Gray <floridabelle2021@protonmail.com>
2023-08-04 04:16:28 -0400

    fixed connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,21 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/neighBorrow');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/neighBorrow');
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
 
 
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/neighBorrow', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to MongoDB!");
+});
+
+module.exports = db;
